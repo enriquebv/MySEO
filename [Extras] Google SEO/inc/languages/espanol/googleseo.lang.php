d1c4e54daa317691669912e379eccc37f901fccc
BitLiberal <bit.liberal.contacto@gmail.com>
2015-01-06 15:29:47 +0100

    Extras para mejorar Google SEO

---
+++ [Extras] Google SEO/inc/languages/espanol/googleseo.lang.php
@@ -0,0 +1,12 @@
++<?php
++
++$l['googleseo_404_notfound'] = "<b>Error 404</b><br />Página no encontrada";
++$l['googleseo_404_widget'] = "<script type=\"text/javascript\">\nvar GOOG_FIXURL_LANG = 'es';\nvar GOOG_FIXURL_SITE = '{1}';\n</script>\n<script type=\"text/javascript\" src=\"http://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js\"></script>";
++$l['googleseo_404_wol'] = "Viendo una <a href=\"{1}\">Página de Error</a>";
++$l['googleseo_meta_page'] = "Página";
++$l['googleseo_sitemap_disabledorinvalid'] = "Mapa del Sitio deshabilitado o no disponible.";
++$l['googleseo_sitemap_emptyorinvalid'] = "Mapa del Sitio vacío o no disponible.";
++$l['googleseo_sitemap_pageinvalid'] = "Página de Mapa del Sitio no disponible.";
++$l['googleseo_sitemap_wol'] = "Buscando el archivo <a href=\"{1}\">XML del Mapa del Sitio</a>";
++
++?>
